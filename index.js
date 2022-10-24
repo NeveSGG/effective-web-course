fd32a5d9c703b333a23d47a024539827933f1fbd
NeveSGG <neverovsg@gmail.com>
2022-10-24 11:16:22 +0600

    feat: responsive header and scroll handler index.html:     new navigation for mobile devices index.css:     media breakpoints and styling for different devices index.js:     scripts for header (drop shadow dissapears on top of the page, mobile menu pop up on clicking burger button)

--- index.js
+++ index.js
@@ -1,60 +1,126 @@
-// Function for handling scroll
-const scrollHandler = () => {
+const doc = document.documentElement;
+const w = window;
 
-  const doc = document.documentElement;
-  const w = window;
+let prevScroll = w.scrollY || doc.scrollTop;
+let curScroll;
+let direction = 0;
+let prevDirection = 0;
 
-  let prevScroll = w.scrollY || doc.scrollTop;
-  let curScroll;
-  let direction = 0;
-  let prevDirection = 0;
+const header = document.getElementById('site-header');
+const checkbox = document.getElementById('checkboxId');
+const mobileNavigation = document.getElementById('mobileNavigation');
+const logo = document.getElementById('logo');
+const headerButton = document.getElementById('headerButton');
 
-  const header = document.getElementById('site-header');
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
-  const checkScroll = () => {
-    /*
-    ** If header is on top then shadow will be removed
-    */
-
-    if (w.scrollY === 0) {
-      header.classList.add('Header--onTop');
-    }
-    else {
-      header.classList.remove('Header--onTop');
-    }
-
-    /*
-    ** Direction of scroll
-    ** 0 - initial, 1 - up, 2 - down
-    */
-
-    curScroll = w.scrollY || doc.scrollTop;
-    if (curScroll > prevScroll) { 
-      //scrolled up
-      direction = 2;
-    }
-    else if (curScroll < prevScroll) { 
-      //scrolled down
-      direction = 1;
-    }
-    if (direction !== prevDirection) {
-      toggleHeader(direction, curScroll);
-    }
-    prevScroll = curScroll;
-  };
-
-  const toggleHeader = (direction, curScroll) => {
-    if (direction === 2 && curScroll > 50) { 
-      header.classList.add('hidden');
-      prevDirection = direction;
-    }
-    else if (direction === 1) {
-      header.classList.remove('hidden');
-      prevDirection = direction;
-    }
-  };
-  
-  window.addEventListener('scroll', checkScroll);
+function preventDefault(e) {
+  e.preventDefault();
 }
 
-scrollHandler();
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+
+
+const checkScroll = () => {
+  /*
+  ** If header is on top then shadow will be removed
+  */
+
+  if (w.scrollY === 0 || doc.scrollTop === 0 ) {
+    header.classList.add('Header--onTop');
+  }
+  else {
+    header.classList.remove('Header--onTop');
+  }
+
+  /*
+  ** Direction of scroll
+  ** 0 - initial, 1 - up, 2 - down
+  */
+
+  curScroll = w.scrollY || doc.scrollTop;
+  if (curScroll > prevScroll) { 
+    //scrolled up
+    direction = 2;
+  }
+  else if (curScroll < prevScroll) { 
+    //scrolled down
+    direction = 1;
+  }
+  if (direction !== prevDirection) {
+    toggleHeader(direction, curScroll);
+  }
+  prevScroll = curScroll;
+};
+
+const toggleHeader = (direction, curScroll) => {
+  if (direction === 2 && curScroll > 50) { 
+    header.classList.add('hidden');
+    prevDirection = direction;
+  }
+  else if (direction === 1) {
+    header.classList.remove('hidden');
+    prevDirection = direction;
+  }
+};
+
+// Function for handling scroll
+const abcdef = () => {
+
+  const scrollHandler = () => {
+    if (checkbox.checked) {
+      window.removeEventListener('scroll', checkScroll);
+      mobileNavigation.classList.remove('closed');
+      header.classList.add('opened');
+      logo.classList.add('onTop');
+      headerButton.classList.add('onTop');
+      disableScroll();
+    } else {
+      enableScroll();
+      logo.classList.remove('onTop');
+      headerButton.classList.remove('onTop');
+      header.classList.remove('opened');
+      mobileNavigation.classList.add('closed');
+      window.addEventListener('scroll', checkScroll);
+    }
+  }
+
+  window.addEventListener('scroll', checkScroll);
+  checkbox.addEventListener('change', scrollHandler);
+}
+
+abcdef();
+
