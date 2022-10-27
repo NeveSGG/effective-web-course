cd4b6b7f09d9b63b90af3e6eaf7bdd6cef931b3f
NeveSGG <neverovsg@gmail.com>
2022-10-28 00:45:36 +0600

    fix: index.css devided into two files - css normalizer and variables are moved to another file - html code validated - html, css, js files beatified

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ let curScroll;
 let direction = 0;
 let prevDirection = 0;
 
-const header = document.getElementById('site-header');
-const checkbox = document.getElementById('checkboxId');
-const mobileNavigation = document.getElementById('mobileNavigation');
-const logo = document.getElementById('logo');
-const headerButton = document.getElementById('headerButton');
+const header = document.getElementById("site-header");
+const checkbox = document.getElementById("checkboxId");
+const mobileNavigation = document.getElementById("mobileNavigation");
+const logo = document.getElementById("logo");
+const headerButton = document.getElementById("headerButton");
 
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
 function preventDefault(e) {
   e.preventDefault();
@@ -28,55 +28,58 @@ function preventDefaultForScrollKeys(e) {
 // modern Chrome requires { passive: false } when adding event
 var supportsPassive = false;
 try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
 
 var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+var wheelEvent =
+  "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
 
 // call this to Disable
 function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
 }
 
 // call this to Enable
 function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
 }
-
-
 
 const checkScroll = () => {
   /*
-  ** If header is on top then shadow will be removed
-  */
+   ** If header is on top then shadow will be removed
+   */
 
-  if (w.scrollY === 0 || doc.scrollTop === 0 ) {
-    header.classList.add('Header--onTop');
-  }
-  else {
-    header.classList.remove('Header--onTop');
+  if (w.scrollY === 0 || doc.scrollTop === 0) {
+    header.classList.add("Header--onTop");
+  } else {
+    header.classList.remove("Header--onTop");
   }
 
   /*
-  ** Direction of scroll
-  ** 0 - initial, 1 - up, 2 - down
-  */
+   ** Direction of scroll
+   ** 0 - initial, 1 - up, 2 - down
+   */
 
   curScroll = w.scrollY || doc.scrollTop;
-  if (curScroll > prevScroll) { 
+  if (curScroll > prevScroll) {
     //scrolled up
     direction = 2;
-  }
-  else if (curScroll < prevScroll) { 
+  } else if (curScroll < prevScroll) {
     //scrolled down
     direction = 1;
   }
@@ -87,40 +90,37 @@ const checkScroll = () => {
 };
 
 const toggleHeader = (direction, curScroll) => {
-  if (direction === 2 && curScroll > 50) { 
-    header.classList.add('hidden');
+  if (direction === 2 && curScroll > 50) {
+    header.classList.add("hidden");
     prevDirection = direction;
-  }
-  else if (direction === 1) {
-    header.classList.remove('hidden');
+  } else if (direction === 1) {
+    header.classList.remove("hidden");
     prevDirection = direction;
   }
 };
 
 // Function for handling scroll
-const abcdef = () => {
-
+const scrollHandling = () => {
   const scrollHandler = () => {
     if (checkbox.checked) {
-      window.removeEventListener('scroll', checkScroll);
-      mobileNavigation.classList.remove('closed');
-      header.classList.add('opened');
-      logo.classList.add('onTop');
-      headerButton.classList.add('onTop');
+      window.removeEventListener("scroll", checkScroll);
+      mobileNavigation.classList.remove("closed");
+      header.classList.add("opened");
+      logo.classList.add("onTop");
+      headerButton.classList.add("onTop");
       disableScroll();
     } else {
       enableScroll();
-      logo.classList.remove('onTop');
-      headerButton.classList.remove('onTop');
-      header.classList.remove('opened');
-      mobileNavigation.classList.add('closed');
-      window.addEventListener('scroll', checkScroll);
+      logo.classList.remove("onTop");
+      headerButton.classList.remove("onTop");
+      header.classList.remove("opened");
+      mobileNavigation.classList.add("closed");
+      window.addEventListener("scroll", checkScroll);
     }
-  }
+  };
 
-  window.addEventListener('scroll', checkScroll);
-  checkbox.addEventListener('change', scrollHandler);
-}
+  window.addEventListener("scroll", checkScroll);
+  checkbox.addEventListener("change", scrollHandler);
+};
 
-abcdef();
-
+scrollHandling();
